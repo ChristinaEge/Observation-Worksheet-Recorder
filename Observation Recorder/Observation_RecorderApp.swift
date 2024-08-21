ec03d228bef21e4c29db3eb2b8431ecf15b0ed05
Christina Ege <Christina_Ege@icloud.com>
2024-08-21 13:55:35 -0700

    Add files via upload

---
+++ Observation Recorder/Observation_RecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Observation_RecorderApp.swift
+//  Observation Recorder
+//
+//  Created by Christina on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Observation_RecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Observation RecorderUITests/Observation_RecorderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Observation_RecorderUITestsLaunchTests.swift
+//  Observation RecorderUITests
+//
+//  Created by Christina on 6/8/24.
+//
+
+import XCTest
+
+final class Observation_RecorderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
